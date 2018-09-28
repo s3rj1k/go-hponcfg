51e61f55a70ae563d93788a0457580ad454fd925
s3rj1k <evasive.gyron@gmail.com>
2018-09-28 17:21:40 +0300

    replace non-ASCII symbols with space

--- bmc.go
+++ bmc.go
@@ -2,12 +2,16 @@ package hponcfg
 
 import (
 	"encoding/xml"
+	"regexp"
 )
 
 // GetBMCData - process BMC(GET_FW_VERSION) output of hponcfg
 func GetBMCData(data []byte) (GetFWVersion, error) {
 
 	var bmcData GetFWVersion
+
+	// replace non-ASCII symbols with space
+	data = regexp.MustCompile("[[:^ascii:]]").ReplaceAllLiteral(data, []byte(" "))
 
 	err := xml.Unmarshal(data, &bmcData)
 	if err != nil {
